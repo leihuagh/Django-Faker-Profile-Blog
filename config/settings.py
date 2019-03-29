84cdb15a133540e7ba8741d565b913cc173daeed
Lei Hua <lhua@example.com>
2019-03-28 21:38:13 -0400

    basic register

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'core.apps.CoreConfig',
     'authentication.apps.AuthenticationConfig',
     'users.apps.UsersConfig',
--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from home.views import index
 from company.views import about, contact
+from users.views import register
 
 urlpatterns = [
     path('', index, name='index'),
@@ -10,4 +11,5 @@ urlpatterns = [
     path('login', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout', LogoutView.as_view(
         template_name='logout.html'), name='logout'),
+    path('register', register, name='register')
 ]
