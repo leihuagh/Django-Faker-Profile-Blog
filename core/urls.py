107ed9779005efbf0750284e2a9f2038f8adc3f8
Lei Hua <lhua@example.com>
2019-03-28 20:44:26 -0400

    basic about and contact

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from home.views import index
+from company.views import about, contact
 
 urlpatterns = [
     path('', index, name='index'),
+    path('about', about, name='about'),
+    path('contact', contact, name='contact'),
 ]
